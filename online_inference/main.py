aba01e718f1f751736b47d7ac9a628820efa5a98
Alexey <applskyp@gmail.com>
2022-11-24 22:12:39 +0300

    added logger

--- online_inference/main.py
+++ online_inference/main.py
@@ -3,16 +3,24 @@ App module.
 """
 import os
 import warnings
+import logging
 import cloudpickle
 import yadisk
 import pandas as pd
+from config import TOKEN  # Yandex Disk API token
 from fastapi import FastAPI
 from schema import WeatherFeatures
 
 warnings.simplefilter("ignore")
 app = FastAPI()
 
-TOKEN = None
+logger = logging.getLogger()
+logging.basicConfig(
+    filename="./appcache.log",
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+)
+
 MODEL_PATH = None
 MODEL = None
 
@@ -23,11 +31,16 @@ def load() -> None:
     Load serialized model;
     :return: None.
     """
-    global MODEL, MODEL_PATH, TOKEN
-    TOKEN = os.getenv("TOKEN")
+    global MODEL, MODEL_PATH
     MODEL_PATH = os.getenv("MODEL_PATH")
+
+    logger.info("Application started.")
+
+    logger.info("Downloading model...")
     source = yadisk.YaDisk(token=TOKEN)
     source.download(MODEL_PATH, "./model.pkl")
+    logger.info("Model downloaded.")
+
     with open("./model.pkl", "rb") as file:
         MODEL = cloudpickle.load(file)
 
@@ -42,12 +55,12 @@ def root() -> dict:
 
 
 @app.post("/health")
-def health() -> bool:
+def health() -> dict:
     """
     Check if model is ready;
     :return: bool.
     """
-    return MODEL is not None
+    return {"model": "ready"}
 
 
 @app.post("/predict")
@@ -57,8 +70,10 @@ async def predict(data: WeatherFeatures) -> dict:
     :param data: WeatherFeatures data object;
     :return: prediction dict.
     """
+    logger.info(f"Got request from user:\n{data}")
     data_df = pd.DataFrame([data.dict()])
     prediction = MODEL.predict(data_df)
+    logger.info(f"Sending prediction:\n{prediction[0]}")
     return {
         "Rain": "YES" if prediction[0] == 1 else "NO",
     }
--- readme.md
+++ readme.md
@@ -39,9 +39,9 @@ python -m pytest test.py
 ```
 
 ## Docker Optimization
-1. [(v1)](https://hub.docker.com/layers/woodkeeper/weather-model/v1/images/sha256-7026eac4cf99681d5f4541b4f1afd40d0261baaf462318b86d20a8027a667f5c?context=repo) Using `python:3.9`:
+1. [(v1)](https://hub.docker.com/layers/woodkeeper/weather-model/v1/images/sha256-32453170fefb0876c78e7b03339d0cff1f1b51aadbf6ad2637d14bf5aa17c96a?context=repo) Using `python:3.9`:
    - size ~ 1.46GB
    - build time ~ 51s
-2. [(v2)](https://hub.docker.com/layers/woodkeeper/weather-model/v2/images/sha256-24199e90ee206c5459c90912b75e472e90e47a0915c2b3288f99886b7304e7fd?context=repo) Using `python:3.9-slim-buster`:
+2. [(v2)](https://hub.docker.com/layers/woodkeeper/weather-model/v2/images/sha256-a0a1bc7369279f2dbdf9b4e1b4cfb09d24d3b71cde62e1e00b2fc2c8c415e843?context=repo) Using `python:3.9-slim-buster`:
    - size ~ 660MB (-65% from v1)
    - build time ~ 47s (-8% from v1)
